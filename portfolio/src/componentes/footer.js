114a92d4276994ea8e860ad4d2dec90b6992980d
SlavikBlxt4 <158159514+SlavikBlxt4@users.noreply.github.com>
2024-02-06 20:08:29 +0100

    punto de control

--- portfolio/src/componentes/footer.js
+++ portfolio/src/componentes/footer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import linkedin from '../img/linkedin.png';
+import github from '../img/github.png';
+import instagram from '../img/instagram.png';
+import whatsapp from '../img/whatsapp.png';
 
 const Footer = () => {
     return(
@@ -6,32 +10,40 @@ const Footer = () => {
         <div className="message">
             <p>Get in Touch With Me</p>
         </div>
-        <div className="mail">
-            <a href="mailto: slavikiftodii@gmail.com">slavikiftodii@gmail.com</a> 
-        </div>
+
         <div className="flex-container">
-            <div className="name-footer">
-                <p>SLAVIK</p>
+            <div className="mail">
+                <a href="mailto: slavikiftodii@gmail.com">slavikiftodii@gmail.com</a> 
             </div>
             <div className="social">
-                <div className="phone-number">
-                        <ul>
-                            <li><a href="tel:+34 642 23 92 16">+34 642 23 92 16</a></li>
-                        </ul>
-                    </div>
                 <div className="social-media">
                     <ul>
                         <li>
-                          <a href="https://www.linkedin.com/in/viacheslav-iftodii/" target="_blank" rel="noopener noreferrer">
-                            <img src="./img/" alt="LinkedIn" />
-                          </a>
+                            <a href="https://www.linkedin.com/in/viacheslav-iftodii/" target="_blank" rel="noopener noreferrer">
+                                <img src={linkedin} alt="LinkedIn"/> 
+                            </a>
                         </li>
-                        <li><a href="https://github.com/SlavikBlxt4" target="_blank" rel="noopener noreferrer">Github</a></li>
-                        <li><a href="https://instagram.com/slaaviik_" target="_blank" rel="noopener noreferrer">Instagram</a></li>
-                        <li><a href="https://wa.me/+34642239216" target="_blank" rel="noopener noreferrer">WhatsApp</a></li>
+                        <li>
+                            <a href="https://github.com/SlavikBlxt4" target="_blank" rel="noopener noreferrer">
+                                <img src={github} alt="GitHub"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://instagram.com/slaaviik_" target="_blank" rel="noopener noreferrer">
+                                <img src={instagram} alt="Instagram"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://wa.me/+34642239216" target="_blank" rel="noopener noreferrer">
+                                <img src={whatsapp} alt="WhatsApp"/>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
+        </div>
+        <div className="line-separator">
+            <hr/>
         </div>
             <div className="copyright">
                 <ul>
