15cf5135a72702d49164977c86f4436f041241e4
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-08 13:18:59 +0100

    ANIMACION HECHA!!!!!!!!!!!!!!!!

---
+++ portfolio/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Header from './componentes/header';
+import Footer from './componentes/footer';
+import Start from './paginas/Start';
+import AboutMe from './paginas/AboutMe';
+import Contact from './paginas/Contact';
+import Main from './paginas/Main';
+import ProjectDetail from './paginas/ProjectDetail';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './App.css';
+
+const Layout = ({ children }) => {
+  const location = useLocation();
+
+  return (
+    <div>
+      <Header />
+      <TransitionGroup>
+        <CSSTransition key={location.key} classNames="page" timeout={300}>
+          {children}
+        </CSSTransition>
+      </TransitionGroup>
+      {location.pathname !== '/' && <Footer />}
+    </div>
+  );
+};
+
+const App = () => {
+  const [page, setPage] = useState('/');
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout><Start setPage={setPage} /></Layout>} />
+        <Route path="/aboutme" element={<Layout><AboutMe setPage={setPage} /></Layout>} />
+        <Route path="/contact" element={<Layout><Contact setPage={setPage} /></Layout>} />
+        <Route path="/home" element={<Layout><Main setPage={setPage} /></Layout>} />
+        <Route path="/projects" element={<Layout><ProjectDetail setPage={setPage} /></Layout>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
